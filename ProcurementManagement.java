a1b81a07f5756bac65c14298f73db4cd04ebc2cd
Amnaaaaa <81854874+Amnaaaaa@users.noreply.github.com>
2021-04-16 15:24:34 +0500

    comit

--- ProcurementManagement.java
+++ ProcurementManagement.java
@@ -3,22 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package procurementmanagement;
 /**
  *
  * @author hp
  */
-public class Driver {
+public class ProcurementManagement {
     public static void main(String[] args){
         
        Employee emp=new Employee();
        Accessories acc=new Accessories();
        Receipt rec =new Receipt();
        
-        
-       
-        
-        
-    }
+       Login l=new Login();
+         l.setVisible(true);
+         
+         
+         
+         Menu m=new Menu();
+         m.setVisible(true);
+           }
     
 }
