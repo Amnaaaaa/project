1d14ce416c31a38b3bcf70aaad3d54a76abb980b
Amnaaaaa <81854874+Amnaaaaa@users.noreply.github.com>
2021-04-16 15:26:38 +0500

    commit

--- Accessories.java
+++ Accessories.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package procurementmanagement;
 /**
  *
  * @author hp
  */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
